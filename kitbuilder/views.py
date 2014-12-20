52282c32ab10f6bab299a2fd91028b1c4eeb9ea0
Brandon Antonelli <bantonelli07@gmail.com>
2014-12-20 02:48:18 -0500

    Style a mockup for the 2nd slide in the kitbuilder

--- kitbuilder/views.py
+++ kitbuilder/views.py
@@ -7,6 +7,8 @@ class KitBuilder(View):
     # Main Kit builder AngularJS/Ember App
     def get(self, request):
         li_list = [x for x in range(40)]
+        li_list2 = [x for x in range(10)]
+        section_list = [x for x in range(7)]
         tags = ["Ambient", "Hard", "Gritty", "Tyson"]
         return render_to_response('KitBuilder/kitbuilder.html', locals())
 
